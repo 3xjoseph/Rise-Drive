dac041ede9a9fda44fc8c2a94b84e217bf05246b
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-08-17 23:11:38 +0800

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    [SerializeField] float mainThrust;
+    [SerializeField] float rotateThrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust() 
+    {    
+        //Gets Thrust
+        if (Input.GetKey(KeyCode.Space)) 
+        {
+            myRigidbody.AddRelativeForce((Vector3.up * mainThrust) * Time.deltaTime);
+        }
+    }
+
+    void ProcessRotation() 
+    {
+        //Rotate Rocket
+        if (Input.GetKey(KeyCode.D ) && !Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(-rotateThrust);
+        }
+        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) 
+        {
+            ApplyRotation(rotateThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        myRigidbody.freezeRotation = true;
+        transform.Rotate((Vector3.forward * rotationThisFrame) * Time.deltaTime);
+        myRigidbody.freezeRotation = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Rise-Drive
+ A 3D Platformer Game where you take on as a journey on your rocketship and get to your destination.
