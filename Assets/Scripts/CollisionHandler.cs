e91f26badd491448330ca7de059e17ab8ebead54
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-08-19 14:23:02 +0800

    44. Relaod Using SceneManager
    - Added a SceneManager on "CollisionHandler" script that reloads the scene when the user collides with an untagged GameObject.
    - Use SceneManager class by declaring the UnityEngine.SceneManagement.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {   
@@ -16,9 +17,13 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("Rocket Refueld");
                 break;
             default:
-                Debug.Log("Game Over");
+                ReloadLevel();
                 break;
-         }
-
+        }
+    }
+    static void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
     }
 }
