8b292d5a9a994196cb1a5287cc9ce6a7683c7eca
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-08-22 19:44:39 +0800

    57. Mathf For Oscillation

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,8 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPosition;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,16 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //continually growing over time.
+        float cycles = Time.time / period;
+        
+        //Constant value of 6.283.
+        const float tau = Mathf.PI * 2;
+        //Going from -1 to 1.
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        //Recalculated to go from 0 to 1 so it's cleaner.
+        movementFactor = (rawSinWave + 1f) / 2;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
